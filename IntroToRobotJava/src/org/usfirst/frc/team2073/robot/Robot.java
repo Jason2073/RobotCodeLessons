c7429adf69c9bcdbe8db3028f8e2393a7b4ee7d5
Jason Stanley <stanlteytechrepair@gmail.com>
2017-12-08 18:55:15 -0800

    added comments for more explanation

--- IntroToRobotJava/src/org/usfirst/frc/team2073/robot/Robot.java
+++ IntroToRobotJava/src/org/usfirst/frc/team2073/robot/Robot.java
@@ -24,12 +24,16 @@ public class Robot extends SampleRobot {
 	@Override
 	public void operatorControl() {
 		while (true) {
+//			if you press button 1, set speed to .5;
 			if (controller.getRawButton(1)) {
-				speed = .5;
+				speed = .5;	
+//				otherwise, if you press button 2, set speed to .5 in the other direction;
 			} else if (controller.getRawButton(2)) {
 				speed = -.5;
+//				if you aren't pressing button 1 or 2, and you are moving the joystick, set speed = the joystick value (1 is full forward, -1 is full back)
 			} else if (controller.getY() != 0) {
 				speed = controller.getY();
+//				if you press button 3, do this series of commands. 
 			} else if (controller.getRawButton(3)) {
 				motor.set(.5);
 				Timer.delay(1);
@@ -41,11 +45,13 @@ public class Robot extends SampleRobot {
 				Timer.delay(.5);
 				motor.set(1);
 				Timer.delay(.5);
+//				if you are not pressing buttons 1,2 or 3, and not moving the joystick, don't move
 			} else {
 				speed = 0;
 			}
-
+// this line actually tells the motor to go the speed that we set earlier.
 			motor.set(speed);
+//			This delay is the cycle time the robot operates on.
 			Timer.delay(.005);
 		}
 	}
@@ -54,6 +60,8 @@ public class Robot extends SampleRobot {
 	public void test() {
 		double motorSpeed = 0;
 		while (true) {
+//			from what you learned earlier, what should this code do?
+			
 			if (controller.getPOV() == 0) {
 				motorSpeed = 1;
 			} else if (controller.getPOV() == 45) {
